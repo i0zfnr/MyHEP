da9c1874b4f9e9975359669846818ea93d2b24d3
i0zfnr <irfanhafizul123@gmail.com>
2026-08-27 22:12:45 +0800

    fix: emit PDF worker as JavaScript

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,24 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    const sourceName = assetInfo.originalFileNames?.[0]
+                        ?? assetInfo.names?.[0]
+                        ?? assetInfo.name
+                        ?? '';
+
+                    if (sourceName.endsWith('.mjs')) {
+                        return 'assets/[name]-[hash].js';
+                    }
+
+                    return 'assets/[name]-[hash][extname]';
+                },
+            },
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/css/design-system.css', 'resources/js/app.js', 'resources/js/certificate-template-editor.js'],
